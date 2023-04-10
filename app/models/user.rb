e7d19ae218ce7c4b0b0c7002d8ebcddbbd7802c2
Nobu07t <nobu0720np@gmail.com>
2023-04-10 19:53:03 +0900

    ユーザー管理機能全体の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,12 +13,12 @@ class User < ApplicationRecord
   validates :birth_date, presence: true
   
   # お名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が必須であること
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/, message: 'は全角で入力してください' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/, message: 'は全角で入力してください' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください' }
 
 
   # お名前カナ(全角)は、全角（カタカナ）での入力が必須であること。
-  validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}ー]+\z/, message: 'は全角カタカナで入力してください' }
-  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}ー]+\z/, message: 'は全角カタカナで入力してください' }
+  validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/, message: 'は全角カタカナで入力してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/, message: 'は全角カタカナで入力してください' }
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
 
+   context '新規登録できるとき' do
+    it '全項目が存在すれば登録できる' do
+      expect(@user).to be_valid
+   end
+    end
+   context '新規登録できないとき' do
     it 'nicknameが空では登録できない' do
       @user.nickname = ""
       @user.valid?
@@ -46,9 +52,23 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
     end
 
-    it 'passwordは半角英数字混合でなければ登録できない' do
+    it 'passwordは英字のみのパスワードでは登録できない' do
+      @user.password = 'abcdef'
+        @user.password_confirmation = 'abcdef'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password is invalid"
+    end
+
+    it 'passwordは数字のみのパスワードでは登録できない' do
       @user.password = '000000'
         @user.password_confirmation = '000000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password is invalid"
+    end
+
+    it 'passwordは全角文字を含むパスワードでは登録できない' do
+      @user.password = 'ああああああ'
+        @user.password_confirmation = 'ああああああ'
         @user.valid?
         expect(@user.errors.full_messages).to include "Password is invalid"
     end
@@ -97,13 +117,13 @@ RSpec.describe User, type: :model do
     end
 
     it 'first_name_kanaが全角（カタカナ）でなければ登録できない' do
-      @user.first_name_kana = "太郎"
+      @user.first_name_kana = "ﾀﾛｳ"
       @user.valid?
       expect(@user.errors.full_messages).to include "First name kana は全角カタカナで入力してください"
     end
 
     it 'last_name_kanaが全角（カタカナ）でなければ登録できない' do
-      @user.last_name_kana = "山田"
+      @user.last_name_kana = "ﾔﾏﾀﾞ"
       @user.valid?
       expect(@user.errors.full_messages).to include "Last name kana は全角カタカナで入力してください"
     end
@@ -113,7 +133,7 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include "Birth date can't be blank"
     end
-
+   end
   end
 end
 
