b92b3166666262846a583a3b0ce34d079c1b19b4
Nobu07t <nobu0720np@gmail.com>
2023-04-16 16:42:21 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,9 +27,9 @@ def edit
 end
 
 def update
-  item = Item.find(params[:id])
-  if item.update(item_params)
-      redirect_to item_path(item)
+  @item = Item.find(params[:id])
+  if @item.update(item_params)
+      redirect_to item_path(@item)
   else
     render :edit
   end
